1fd02c240a9e6344a53cf432515948474f9475a0
abc <abc@example.com>
2022-01-14 11:42:26 +0530

    third commit

---
+++ src/main/java/com/example/sample/SampleController.java
@@ -0,0 +1,18 @@
+package com.example.sample;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class SampleController {
+	
+	 @GetMapping(value="/hello")
+	    public String getAll(){
+			 
+	         return "welcome to sample application";
+		 }
+
+}
